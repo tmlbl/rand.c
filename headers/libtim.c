65ffa9bd999d99749a43ae26514fea88e7492e97
tmlbl <tim.lebel@gmail.com>
2015-07-26 09:15:37 -0700

    Fixed error

--- headers/libtim.c
+++ headers/libtim.c
@@ -1,10 +1,13 @@
 #include "libtim.h"
 #include <stdio.h>
+#include <string.h>
 
 char* greet(char* name)
 {
-  char* msg;
-  sprintf(msg, "Hello, %s\n", name);
-  return msg;
+  char* msg = "Hello, %s\n";
+  long msg_len = strlen(msg) + strlen(name);
+  char buf[msg_len];
+  snprintf(buf, msg_len - 1, msg, name);
+  return buf;
 }
 
